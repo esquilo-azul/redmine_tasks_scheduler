366fb78b940d72e4caa0afbca8eb90bdb6dc11e9
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2016-11-22 09:48:14 -0300

    Plugin "redmine_tasks_scheduler".

---
+++ init.rb
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+require 'redmine'
+
+Redmine::Plugin.register :redmine_tasks_scheduler do
+  name 'Redmine Tasks Scheduler'
+  author 'eduardobogoni@gmail.com'
+  description 'Scheduler for rake tasks on Redmine.'
+  version '0.0.1'
+end
