8a06385484da739854e1df460dac7f84cef6e2b9
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2017-06-08 18:36:51 -0300

    Link "Tarefas Agendadas" na área administrativa aponta para /scheduled_tasks/status. Refs #2493.

--- init.rb
+++ init.rb
@@ -17,7 +17,7 @@ Rails.configuration.to_prepare do
   require_dependency 'redmine_tasks_scheduler/patches/tasks_scheduler_controller_patch'
   require_dependency 'redmine_tasks_scheduler/hooks/add_assets'
   Redmine::MenuManager.map :admin_menu do |menu|
-    menu.push :scheduled_tasks, { controller: 'tasks_scheduler_daemon', action: 'index', id: nil },
+    menu.push :scheduled_tasks, { controller: 'scheduled_tasks', action: 'status', id: nil },
               caption: :'activerecord.models.scheduled_task.other',
               if: proc { GroupPermission.permission?('scheduled_tasks') }
   end
