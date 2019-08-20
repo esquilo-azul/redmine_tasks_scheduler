317992fdb14734187d0cb61ef9310e34c937c2fb
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-08-20 18:14:08 -0300

    Update plugin "redmine_nonproject_modules" to '0.2.1'.

--- config/initializers/000_dependencies.rb
+++ config/initializers/000_dependencies.rb
@@ -2,5 +2,5 @@
 
 Redmine::Plugin.post_register :redmine_events_manager do
   # Source: https://github.com/esquilo-azul/redmine_nonproject_modules
-  requires_redmine_plugin(:redmine_nonproject_modules, version_or_higher: '0.2.0')
+  requires_redmine_plugin(:redmine_nonproject_modules, version_or_higher: '0.2.1')
 end
