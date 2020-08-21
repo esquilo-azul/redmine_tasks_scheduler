c23f01c71065493f3bab2970129a9f9f39d18365
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-08-20 21:22:27 -0300

    config/initializers/000_dependencies.rb: refact.

--- config/initializers/000_dependencies.rb
+++ config/initializers/000_dependencies.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-Redmine::Plugin.post_register :redmine_events_manager do
+Redmine::Plugin.by_path(__FILE__).instance_eval do
   # Source: https://github.com/esquilo-azul/redmine_nonproject_modules
   requires_redmine_plugin(:redmine_nonproject_modules, version_or_higher: '0.2.1')
 end
