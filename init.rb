1fa3a0e7b093e32eb11b653e355d5b8c7dff177f
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-07-02 14:52:04 -0300

    Bump to '0.2.4'.

--- init.rb
+++ init.rb
@@ -6,6 +6,6 @@ Redmine::Plugin.register :redmine_tasks_scheduler do
   name 'Redmine Tasks Scheduler'
   author 'eduardobogoni@gmail.com'
   description 'Scheduler for rake tasks on Redmine.'
-  version '0.2.3'
+  version '0.2.4'
   settings default: { 'daemon_autostart' => '1' }, partial: 'settings/redmine_tasks_scheduler'
 end
