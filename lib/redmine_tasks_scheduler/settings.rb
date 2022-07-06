b6f5e52331490e440296816006f221801fc81304
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-07-06 20:25:58 -0300

    RedmineTasksScheduler::Settings.settings_table_exist?: replace by RedminePluginsHelper::Available.settings?.

--- lib/redmine_tasks_scheduler/settings.rb
+++ lib/redmine_tasks_scheduler/settings.rb
@@ -4,15 +4,9 @@ module RedmineTasksScheduler
   class Settings
     class << self
       def daemon_autostart
-        return false unless settings_table_exist?
+        return false unless ::RedminePluginsHelper::Available.settings?
 
         ::Setting.plugin_redmine_tasks_scheduler[:daemon_autostart] == '1'
-      end
-
-      private
-
-      def settings_table_exist?
-        ::Setting.connection.table_exists? ::Setting.table_name
       end
     end
   end
