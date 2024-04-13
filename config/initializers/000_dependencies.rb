62e58046dc5cb350d06de2f377ae07c097769819
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-04-12 21:17:15 -0300

    dependencies.yaml: convert from "config/initializers/000_dependencies.rb".

--- config/initializers/000_dependencies.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-Redmine::Plugin.by_path(__FILE__).instance_eval do
-  # Source: https://github.com/esquilo-azul/redmine_nonproject_modules
-  requires_redmine_plugin(:redmine_nonproject_modules, version_or_higher: '0.5.2')
-end
