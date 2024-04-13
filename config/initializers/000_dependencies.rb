33c34f7ec75688f2620560ca509ffe79bce1010f
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2024-04-12 21:12:14 -0300

    Plugin "redmine_nonproject_modules": update to '>= 0.5.2'.

--- config/initializers/000_dependencies.rb
+++ config/initializers/000_dependencies.rb
@@ -2,5 +2,5 @@
 
 Redmine::Plugin.by_path(__FILE__).instance_eval do
   # Source: https://github.com/esquilo-azul/redmine_nonproject_modules
-  requires_redmine_plugin(:redmine_nonproject_modules, version_or_higher: '0.3.12')
+  requires_redmine_plugin(:redmine_nonproject_modules, version_or_higher: '0.5.2')
 end
