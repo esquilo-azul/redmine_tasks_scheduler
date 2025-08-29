b71bd5ad41a5708525c7fd4f263411bd1dc9e44b
Eduardo Henrique Bogoni <eduardo.bogoni@trf1.jus.br>
2025-08-29 08:58:00 -0300

    **/*.rb: format with "rubocop".

--- lib/redmine_tasks_scheduler/settings.rb
+++ lib/redmine_tasks_scheduler/settings.rb
@@ -3,7 +3,7 @@
 module RedmineTasksScheduler
   class Settings
     class << self
-      def daemon_autostart
+      def daemon_autostart # rubocop:disable Naming/PredicateMethod
         return false unless ::RedminePluginsHelper::Available.settings?
 
         ::Setting.plugin_redmine_tasks_scheduler[:daemon_autostart] == '1'
