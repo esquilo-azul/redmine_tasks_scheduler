1a040289ef1fc6c7f6905eecc202558106ae631c
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-02-27 18:22:12 -0300

    **/*.rb: normalize requires.

--- config/initializers/250_dependencies.rb
+++
@@ -1,3 +0,0 @@
-# frozen_string_literal: true
-
-require 'tasks_scheduler/engine'
--- lib/redmine_tasks_scheduler/patches/tasks_scheduler_controller_patch.rb
+++ lib/redmine_tasks_scheduler/patches/tasks_scheduler_controller_patch.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'redmine_tasks_scheduler/permissions'
-
 module RedmineTasksScheduler
   module Patches
     module TasksSchedulerControllerPatch
